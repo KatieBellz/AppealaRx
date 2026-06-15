29e0a0a6d5c12164a082d9558719e6ab6147395e
Kathy Rodriguez <KateRodriguez777@gmail.com>
2026-06-14 21:34:35 -0400

    fix index.html restore full working app

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.post('/generate', async (req, res) => {
 const { patientName, insurance, service, denialReason, notes, dob, memberId, claimNumber } = req.body;
